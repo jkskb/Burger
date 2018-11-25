733b74968c06cc6ddfbdab107af453da94b89d11
Kate Ryan <jkskb@yahoo.com>
2018-11-25 14:27:38 -0600

    not working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon, Double Cheese with Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger only mustard');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tribble Cheese, no Onion', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tex-Mex Burger, extra Salsa', true);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger, Grilled Onions, with BBQ Sauce');
